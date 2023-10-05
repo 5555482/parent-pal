9532a9a4411434b575d47770b266810573c35a43
Nataliia Antonova <5555482@gmail.com>
2023-10-05 12:24:57 +0100

    Button validating props

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Button({ children }) {
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  checkTheType: (primary, secondary, success, warning, danger) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!warning);
+
+    if (count > 1) {
+      return new Error(
+        "Only one primary, secondary, success, warning, danger can be used"
+      );
+    }
+  }
+};
 
 export default Button;
