f095bfc9b448f4a81cce14b16475790bf169c31a
Nataliia Antonova <5555482@gmail.com>
2023-10-05 11:50:58 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button success rounded outline>
+          Click here
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Hop
+        </Button>
+      </div>
+      <div>
+        <Button secondery outline>
+          Hide ads
+        </Button>
+      </div>
+      <div>
+        <Button success rounded outline>
+          Hi
+        </Button>
+      </div>
     </div>
   );
 }
