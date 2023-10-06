9d3820d57d8fdcff2da752b73caa23ec094584b6
Nataliia Antonova <5555482@gmail.com>
2023-10-06 21:49:53 +0100

    Add react-icons to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Button from "./Button";
+import { CiAlignLeft, CiDumbbell, CiLineHeight } from "react-icons/ci";
 
 function App() {
   return (
     <div>
       <div>
         <Button success rounded>
+          <CiAlignLeft />
           Click here
         </Button>
       </div>
       <div>
         <Button danger rounded outline>
+          <CiDumbbell />
           Hop
         </Button>
       </div>
@@ -21,6 +24,7 @@ function App() {
       </div>
       <div>
         <Button rounded warning outline>
+          <CiLineHeight />
           Hi
         </Button>
       </div>
