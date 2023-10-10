d3094f678d8cd858f874e9495f78fef1005e6cfc
Nataliia Antonova <5555482@gmail.com>
2023-10-10 13:27:11 +0100

    Adding icons

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
+import { CiSquareChevDown, CiSquareChevLeft } from "react-icons/ci";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = index => {
+    setExpandedIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = (
+      <span>{isExpanded ? <CiSquareChevDown /> : <CiSquareChevLeft />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
