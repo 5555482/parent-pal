cc5a8da9f77b7f37c332a87d0df88c02819c9f14
Nataliia Antonova <5555482@gmail.com>
2023-10-17 14:47:07 +0100

    Listening to forward and back clicks

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,24 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Consumer.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Consumer.Provider>
   );
