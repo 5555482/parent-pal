e4158c916b8ec2c6bc8962c8240b7531e079f192
Nataliia Antonova <5555482@gmail.com>
2023-11-18 20:21:28 +0000

    pull fake data

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Food from "../components/Food"
+import Popular from "../components/Popular"
+
+export default function Home() {
+  return (
+    <div>
+      <Food />
+      <Popular />
+    </div>
+  )
+}
