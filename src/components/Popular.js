079d039bb45369e7aebbd47552dbb97b749e7c30
Nataliia Antonova <5555482@gmail.com>
2023-11-18 20:33:23 +0000

    splide slide

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -16,54 +18,52 @@ function Popular() {
     setPopular(data.recipes);
   };
   return (
-    <div>
-      {popular.map(recipe => {
-        return (
-          <div className="m-4" key={recipe.id}>
-            <h3>Popular Picks</h3>
-            {popular.map(recipe => {
-              return (
-                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="m-4">
+      <h3>Popular Picks</h3>
+      <Splide options={{ perPage: 4 }}>
+        {popular.map(recipe => {
+          return (
+            <SplideSlide>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img className="rounded-t-lg" src={recipe.image} alt="" />
+                </a>
+                <div className="p-5">
                   <a href="#">
-                    <img className="rounded-t-lg" src={recipe.image} alt="" />
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      Noteworthy technology acquisitions 2021
+                    </h5>
                   </a>
-                  <div className="p-5">
-                    <a href="#">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Noteworthy technology acquisitions 2021
-                      </h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {recipe.title}
-                    </p>
-                    <a
-                      href="#"
-                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {recipe.title}
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Read more
+                    <svg
+                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
                     >
-                      Read more
-                      <svg
-                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 10"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M1 5h12m0 0L9 1m4 4L9 9"
-                        />
-                      </svg>
-                    </a>
-                  </div>
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </a>
                 </div>
-              );
-            })}
-          </div>
-        );
-      })}
+              </div>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
     </div>
   );
 }
